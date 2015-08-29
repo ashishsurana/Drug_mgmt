c47031aa1ad86ebc194e1eb1b075c5dc657dc27c
Ashish Surana <ashishsurana@ymail.com>
2015-08-29 11:13:01 +0530

    initial Codes

---
+++ src/Medicine.java
@@ -0,0 +1,17 @@
+
+public class Medicine {
+	private String name;
+	private String company;
+	private String bno;
+	private String exp;
+	private String pdate;
+	private String qty;
+	private String billno;
+	private String from;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Sales.java
@@ -0,0 +1,13 @@
+
+public class Sales {
+	private String pname;
+	private String dame;
+	private String med;
+	private String qty;
+	private String bno;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
