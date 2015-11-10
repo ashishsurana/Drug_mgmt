a1e87f23456ed3db49fe87c78b109d9d58771f98
Ashish Surana <ashishsurana@ymail.com>
2015-11-10 21:52:40 +0530

    Sales completed

--- src/Sales.java
+++ src/Sales.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Sales {
 	private String pname;
@@ -11,83 +15,70 @@ public class Sales {
 	public static void main(String[] args) {
 		final JFrame jFrame = new JFrame();
 		jFrame.setSize(600, 600);
-		jFrame.setVisible(true);
-		jFrame.setLayout(null);
-		jFrame.setTitle("Sales Entry");
 
-		/*JLabel label_ = new JLabel("");
-		label_.setBounds(100, 100, 100, 25);
-		jFrame.add(label_);
-		JTextField textField_ = new JTextField();
-		textField_.setBounds(250,100, 100, 25);
-		jFrame.add(textField_);
-		*/
-
-		JLabel label_name = new JLabel("Medicine Name");
-		label_name.setBounds(100, 100, 200, 25);
+		JLabel label_name = new JLabel("Medicine Name :");
+		label_name.setBounds(100,100,200,25);
 		jFrame.add(label_name);
 		final JTextField textField_name = new JTextField();
-		textField_name.setBounds(250, 100, 100, 25);
+		textField_name.setBounds(250, 100, 200, 25);
 		jFrame.add(textField_name);
 
-		JLabel label_bno = new JLabel("Batch No.");
-		label_bno.setBounds(100, 140, 100, 25);
-		jFrame.add(label_bno);
-		final JTextField textField_bno = new JTextField();
-		textField_bno.setBounds(250,140, 100, 25);
-		jFrame.add(textField_bno);
+		JLabel label_batchno = new JLabel("Batch No :");
+		label_batchno.setBounds(100,140,200,25);
+		jFrame.add(label_batchno);
+		final JTextField textField_batchno = new JTextField();
+		textField_batchno.setBounds(250,140,200,25);
+		jFrame.add(textField_batchno);
 
-		JLabel label_qty = new JLabel("Quantity");
-		label_qty.setBounds(100, 180, 100, 25);
+		JLabel label_qty = new JLabel("Quantity : ");
+		label_qty.setBounds(100,180,200,25);
 		jFrame.add(label_qty);
 		final JTextField textField_qty = new JTextField();
-		textField_qty.setBounds(250, 180, 100, 25);
+		textField_qty.setBounds(250,180,200,25);
 		jFrame.add(textField_qty);
-
-		JLabel label_billno = new JLabel("Bill No.");
-		label_billno.setBounds(100, 220, 100, 25);
-		jFrame.add(label_billno);
-		final JTextField textField_billno = new JTextField();
-		textField_billno.setBounds(250, 220, 100, 25);
-		jFrame.add(textField_billno);
-
-
 
 		JButton cancel = new JButton("Cancel");
 		cancel.setBounds(100, 450, 100, 25);
 		jFrame.add(cancel);
-		cancel.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent actionEvent) {
-				jFrame.dispose();
-			}
-		});
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                jFrame.dispose();
+            }
+        });
+		JButton clear = new JButton("Clear");
+		clear.setBounds(400, 450, 100, 25);
+		jFrame.add(clear);
+		clear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                textField_name.setText(null);
+                textField_batchno.setText(null);
+                textField_qty.setText(null);
+            }
+        });
 
 		JButton submit = new JButton("Submit");
 		submit.setBounds(250, 450, 100, 25);
 		jFrame.add(submit);
 		submit.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent actionEvent) {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String url = new String("jdbc:mysql://127.0.0.1:3306/stock");
+                String user = new String("admin");
+                String pass = new String("password");
 
-			}
-		});
 
-		JButton clear = new JButton("Clear");
-		clear.setBounds(400,450,100,25);
-		jFrame.add(clear);
-		clear.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent actionEvent) {
-				textField_name.setText(null);
-				textField_bno.setText(null);
-				textField_qty.setText(null);
-//				textField_pa.setText(null);
-//				textField_billdate.setText(null);
-				textField_billno.setText(null);
-			}
-		});
+                textField_name.setText(null);
+                textField_qty.setText(null);
 
+
+            }
+        });
+
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setLayout(null);
+		jFrame.setVisible(true);
 	}
 
 }
